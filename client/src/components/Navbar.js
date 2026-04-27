3222a5c14b564e5ecab1b3b8160ed302a0443050
shuling02 <sllin23999@gmail.com>
2026-04-26 20:58:09 -0700

    Logout page confirmation bug fixed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,22 @@
 import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "../styles/navbar.scss";
 
-function Navbar(){
+function Navbar() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        axios.get(
+            `${process.env.REACT_APP_API_URL}/api/profile`,
+            { withCredentials: true }
+        )
+            .then(() => setIsLoggedIn(true))
+            .catch(() => setIsLoggedIn(false));
+    }, []);
+
+
     return (
         <div className="navbar">
             <img src="/logo.png" alt="logo" className="logo" />
@@ -9,9 +24,14 @@ function Navbar(){
             <div className="nav-links">
                 <NavLink to="/dashboard">Home</NavLink>
                 <NavLink to="/profile">Profile</NavLink>
-                <NavLink to="/login">Login</NavLink>
-                <NavLink to="/signup">Sign Up</NavLink>
-                <NavLink to="/logout">Logout</NavLink>
+                {isLoggedIn ? (
+                    <NavLink to="/logout">Logout</NavLink>
+                ) : (
+                    <>
+                        <NavLink to="/login">Login</NavLink>
+                        <NavLink to="/signup">Sign Up</NavLink>
+                    </>
+                )}
             </div>
         </div>
     );
